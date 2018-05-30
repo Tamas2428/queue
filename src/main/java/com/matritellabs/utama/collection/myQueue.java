114c5336d257606a2b76ac6b2b51c20cd5471a7d
tamas2428 <berki.tamas.jozsef@gmail.com>
2018-05-30 20:23:54 +0200

    myQueue.java remove method done

--- src/main/java/com/matritellabs/utama/collection/myQueue.java
+++ src/main/java/com/matritellabs/utama/collection/myQueue.java
@@ -9,7 +9,7 @@ public abstract class myQueue implements Queue {
 
     //int vaiable to store maximum capacity of the Queue
     int lengthOfQueue;
-    int numberOfObjects = 1;
+    int numberOfObjects = 0;
 
     //map collection to store objects
     Map mapOfTheQueue = new HashMap(lengthOfQueue);
@@ -29,7 +29,9 @@ public abstract class myQueue implements Queue {
     @Override
     public boolean add(Object o) {
         if(numberOfObjects < lengthOfQueue) {
-            mapOfTheQueue.put(numberOfObjects, o);
+            //numberOfObjects+1 - we start from zero, and every added element will have previous element key +1 as key
+            mapOfTheQueue.put(numberOfObjects+1, o);
+            //to keep track the number of the objects in the Queue
             numberOfObjects++;
             return true;
         } else {
@@ -44,7 +46,7 @@ public abstract class myQueue implements Queue {
      */
     @Override
     public Object element() {
-        if(numberOfObjects == 1) {
+        if(numberOfObjects == 0) {
             throw new NoSuchElementException();
         } else {
             return mapOfTheQueue.get(1);
@@ -55,21 +57,35 @@ public abstract class myQueue implements Queue {
      * Inserts the specified element into this queue if it is possible to do so immediately without
      violating capacity restrictions.
      * @param o
-     * @return true upon success
+     * @return true upon success, false otherwise
      */
     @Override
     public boolean offer(Object o) {
         if(numberOfObjects < lengthOfQueue) {
             mapOfTheQueue.put(numberOfObjects+1, o);
+            numberOfObjects++;
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Retrieves and removes the head of this queue.
+     * @return NoSuchElementException if this queue is empty
+     */
     @Override
     public Object remove() {
-        return null;
+        if(numberOfObjects != 0) {
+            Object tempObject = mapOfTheQueue.get(1);
+            for(int i = 2; i < lengthOfQueue; i++) {
+                mapOfTheQueue.put(i-1, mapOfTheQueue.get(i));
+            }
+            numberOfObjects--;
+            return tempObject;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
